2285a0f54832655c8d71f0d0ba53ebfebab90578
Maria Fernanda Garcia Monterroso <201800087@colsanjosee.edu.gt>
2020-04-17 14:27:38 -0600

    Se configuró la página.

---
+++ calculadora.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+<title>Resultado</title>
+</head>
+
+<body>
+
+<header>
+<h1>RESPUESTA</h1>
+</header>
+
+<section>
+<?php 
+
+$valoruno=$_POST['1er'];
+$valordos=$_POST['2do'];
+
+$opcion=$_POST['operaciones'];
+
+
+if($opcion == 'suma')
+{
+ $Resultado=($valoruno+$valordos);
+}
+
+else if($opcion == 'resta')
+{
+ $Resultado=($valoruno-$valordos);
+}
+
+else if($opcion == 'mult')
+{
+ $Resultado=($valoruno*$valordos);
+}
+
+else if($opcion == 'divs')
+{
+ $Resultado=($valoruno/$valordos);
+}
+
+?>
+
+<p>
+<?php echo $Resultado; ?>
+</p>
+ 
+</section>
+</body>
+</html>
